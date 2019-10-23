288018a2a47800e79d0fd961acdca0628232066d
ThomasNGit <neronithomas1989@gmail.com>
2019-10-23 13:49:19 +0200

    Chiesto all'utente il suo cognome e ordinati tutti i cognomi in ordine alfabetico

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,21 +5,32 @@
 
 
 //creo array con una lista di cognomi
-var listacognomi = ["Rossi", "Bianchi", "Pinco", "Pallino"]
+var listacognomi = ["rossi", "bianchi", "pinco", "pallino", "zeta", "grieco"]
 
 console.log(listacognomi);
 
 
 //Chiedo il cognome all'utente con un prompt
+var cognomeuser = prompt("Ciao, come ti chiami?")
 
+console.log("Il cognome dell'utente è: ", cognomeuser);
 
 
 //inserisco il cognome dell'utente con un push nella lista dei cognomi
+listacognomi.push(cognomeuser);
 
+console.log("La lista dopo l'inserimento del cognome utente è:", listacognomi);
+
+
+
+//sistemo la lista in ordine alfabetico
+var cognomiordinati = listacognomi.sort();
+
+console.log(cognomiordinati);
 
 
 //stampo la lista in ordine alfabetico
-
+document.getElementById("cognomi").innerHTML = "I cognomi in ordine alfabetico sono: " + cognomiordinati;
 
 
 //scrivo in pagina la posizione del cognome dell'utente nella lista
