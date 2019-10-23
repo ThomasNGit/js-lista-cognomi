dbf253e04b437f0513a6cd5a099790d756c7b89d
ThomasNGit <neronithomas1989@gmail.com>
2019-10-23 13:40:55 +0200

    creato scheletro html e array con i cognomi preimpostati

---
+++ assets/javascript/main.js
@@ -0,0 +1,25 @@
+//Chiedi all’utente il cognome,
+//inseriscilo in un array con altri cognomi
+//stampa la lista ordinata alfabeticamente.
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+
+//creo array con una lista di cognomi
+var listacognomi = ["Rossi", "Bianchi", "Pinco", "Pallino"]
+
+console.log(listacognomi);
+
+
+//Chiedo il cognome all'utente con un prompt
+
+
+
+//inserisco il cognome dell'utente con un push nella lista dei cognomi
+
+
+
+//stampo la lista in ordine alfabetico
+
+
+
+//scrivo in pagina la posizione del cognome dell'utente nella lista
